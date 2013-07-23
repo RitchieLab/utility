5e5bc284e1b7805e3c75f3c2e49b797285206a33
atfrase <atf3@psu.edu>
2013-07-23 17:41:04 +0000

    restored zfile.py from r2981, it was apparently deleted via externals
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/util@2984 9e62ecf6-770d-0410-a9d7-e62a33e3d092

---
+++ trunk/zfile.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import os
+import zlib
+
+
+class zopen(object):
+	
+	def __init__(self, fileName, splitChar="\n", chunkSize=16*1024):
+		self._filePtr = open(fileName,'rb')
+		self._splitChar = splitChar
+		self._chunkSize = chunkSize
+		self._dc = zlib.decompressobj(zlib.MAX_WBITS | 32) # autodetect gzip or zlib header
+		self._text = ""
+		self._lines = list()
+	#__init__()
+	
+	
+	def __del__(self):
+		if self._filePtr:
+			self._filePtr.close()
+	#__del__()
+	
+	
+	def __enter__(self):
+		return self
+	#__enter__()
+	
+	
+	def __exit__(self, excType, excVal, excTrace):
+		pass
+	#__exit__()
+	
+	
+	def __iter__(self):
+		return self
+	#__iter__()
+	
+	
+	def __next__(self):
+		# if lines are still cached from the last read, pop one
+		if len(self._lines) > 0:
+			return self._lines.pop()
+		# if there's data left in the source file, read and decompress another chunk
+		if self._dc:
+			data = self._dc.unused_data
+			if data:
+				self._dc = zlib.decompressobj(zlib.MAX_WBITS | 32) # autodetect gzip or zlib header
+			else:
+				data = self._filePtr.read(self._chunkSize)
+			if data:
+				self._text += self._dc.decompress(data)
+				data = None
+			else:
+				self._text += self._dc.flush()
+				self._dc = None
+		# if there's no text left, we're done
+		if not self._text:
+			raise StopIteration
+		# split the text into lines
+		self._lines = self._text.split(self._splitChar)
+		self._text = ""
+		# if there's more than one line, store the last to combine with the next chunk
+		# (but if there's only one line, and more to read, then keep reading until we get a linebreak)
+		if len(self._lines) > 1:
+			self._text = self._lines.pop()
+		elif self._dc:
+			self._text = self._lines.pop()
+			self._chunkSize *= 2
+			return self.__next__()
+		# reverse the remaining lines into a stack and pop one to return
+		self._lines.reverse()
+		return self._lines.pop()
+	#__next__()
+	
+	
+	def next(self):
+		return self.__next__()
+	#next()
+	
+	
+	def seek(self, offset, whence = 0):
+		if offset != 0:
+			raise Exception("zfile.seek() does not support offsets != 0")
+		self._filePtr.seek(0, whence)
+		self._dc.flush()
+		self._text = ""
+		self._lines = list()
+	#seek()
+	
+#zopen
+
+
+if __name__ == '__main__':
+	import sys
+	for n in xrange(1, len(sys.argv)):
+		with zopen(sys.argv[n]) as zfile:
+			for line in zfile:
+				print line
