62c48aa20f754831c743ab080b2005936d1fd533
Anastasia Lucas <36303235+anastasia-lucas@users.noreply.github.com>
2019-01-07 16:18:35 -0500

    Add range slider

--- personal/ana/scripts/manhattan_dash.py
+++ personal/ana/scripts/manhattan_dash.py
@@ -1,5 +1,5 @@
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
@@ -8,15 +8,17 @@ import numpy as np
 
 ###Formatting
 gdat = pd.read_csv("~/Desktop/projects/plotly_dash_ACTG/ACTG_GWAS_TEST.txt", sep=" ")
+gdat[['A1', 'MAF']] = gdat['A1:MAF'].str.split(':', expand=True)
 gdat['Tissue'] = "GWAS"
 gdat['TissueCategory']= "GWAS"
+gdat = gdat[['PHE', 'SNP', 'CHR:BP', 'A1', 'MAF', 'Interaction', 'N', 'pvalue', 'Tissue', 'TissueCategory']]
 tdat = pd.read_csv("~/Desktop/projects/plotly_dash_ACTG/ACTG_TWAS_TEST_map2.txt", sep=" ")
 dat = gdat.append(tdat, ignore_index=True)
-dat[['CHR:BP', 'POS']] = dat['CHR:BP'].str.split(':', expand=True)
-dat = dat[['PHE', 'SNP', 'CHR:BP', 'POS', 'A1:MAF', 'Interaction', 'N', 'pvalue', 'Tissue', 'TissueCategory']]
-dat.columns = ['PHE', 'SNP', 'CHR', 'POS', 'MAF', 'Interaction', 'N', 'pvalue', 'Tissue', 'TissueCategory']
+dat[['CHR', 'POS']] = dat['CHR:BP'].str.split(':', expand=True)
+dat = dat[['PHE', 'SNP', 'CHR', 'POS', 'A1', 'MAF', 'Interaction', 'N', 'pvalue', 'Tissue', 'TissueCategory']]
 dat['CHR'] = dat.CHR.astype(int)
 dat['POS'] = dat.POS.astype(int)
+dat['MAF'] = dat.MAF.astype(float)
 d_order = dat.sort_values(by=['CHR','POS'])
 d_order['pos_index'] = range(1, len(d_order)+1)
 #Get tick positions
@@ -78,7 +80,20 @@ app.layout = html.Div([
             options = il,
             placeholder = "Select an interaction term..."
         ),
-    ],style={'width': '48%', 'display': 'inline-block'}),
+    ],style={'width': '49%', 'display': 'inline-block'}),
+    #html.H1(children='MAF', style={'width': '45%', 'display': 'inline-block', 'font-size': '18px', 'textAlign': 'center'}), 
+    html.Div([
+        dcc.RangeSlider(
+            id='maf-slider',
+            min=0,
+            max=math.ceil(d_order['MAF'].max()*10)/10,
+            step=0.1,
+            marks={0: str(0), 0.1: str(0.1), 0.2: str(0.2), 0.3: str(0.3), 0.4: str(0.4), 0.5: str(0.5), 0.6: str(0.6), 0.7: str(0.7), 0.8: str(0.8), 0.9: str(0.9), 1: str(1)},
+            value=[0,math.ceil(d_order['MAF'].max()*10)/10],
+            allowCross=False
+            #marks={round(interaction,1): round(interaction, 1) for interaction in np.linspace(0,round(d_order['MAF'].max(), 1),11)}
+        )
+    ],style={'width': '49%', 'display': 'inline-block', 'verticalAlign': 'top', 'horizontalAlign': 'right'}),
 
     html.Div([
         dcc.Dropdown(
@@ -86,13 +101,14 @@ app.layout = html.Div([
             options = [{'label' : k, 'value': k} for k in tcdict.keys()],
             placeholder = "Select a tissue category..."
         ),
-
+        ],style={'width': '49%', 'display': 'inline-block', 'horizontalAlign': 'right'}),
+    html.Div([
         dcc.Dropdown(
             id='tissue-dropdown',
             placeholder = "Select a tissue...",
             multi=True)
 
-    ],style={'width': '48%', 'display': 'inline-block'}),
+    ],style={'width': '49%', 'display': 'inline-block', 'horizontalAlign': 'right'}),
 
     html.Div([
         dcc.Graph(id='twas-graph'),
@@ -112,41 +128,17 @@ def set_tissue_options(selected_category):
 def set_tissue_value(available_options):
     return available_options[0]['value']
 
-@app.callback(Output('gwas-graph', 'figure'), [Input('interaction-dropdown', 'value'), Input('tissue-dropdown', 'value')])
-def update_graph(selected_dropdown_value, selected_dropdown_value2):
-    #Here it would be possible to split df into 9 separate traces and return the list   
-    df = d_order[(d_order.Interaction==selected_dropdown_value) & (d_order.Tissue=="GWAS")]
+@app.callback(Output('gwas-graph', 'figure'), [Input('interaction-dropdown', 'value'), Input('tissue-dropdown', 'value'), Input('maf-slider', 'value')]) #, [State('maf-slider', 'marks')])
+def update_graph(selected_dropdown_value, selected_dropdown_value2, selected_maf):
+    #Here it would be possible to split df into 9 separate traces and return the list
+    #mafvalue = marks[str(selected_maf)]
+    df = d_order[(d_order.Interaction==selected_dropdown_value) & (d_order.Tissue=="GWAS") & (d_order.MAF > selected_maf[0]) & (d_order.MAF < selected_maf[1])]
     #cols = df['PHE'].map(colorsIdx)
     dl = []
     for i in df.PHE.unique():
         dfsub = df[df.PHE==i]
-        dff = {'x': dfsub.pos_index, 'y': dfsub.logp, 'mode': 'markers', 'text': dfsub.SNP + '\n' + dfsub.MAF, 'name': i}
-        dl.append(dff)
-    return {
-        'data': dl,
-        'layout': {'shapes': sl, 'title': 'GWAS Plot', 'showlegend': True, 'xaxis': {'range': [xmin, xmax], 'ticktext': lims.chrmin, 'tickvals': lims.av, 'showgrid': False}, 'yaxis': {'range': [ymin,ymax], 'title': '-log10(pvalue)'}, 'hovermode': 'closest'}
-    }
-@app.callback(
-    Output('tissue-dropdown', 'options'),
-    [Input('tissue-category-dropdown', 'value')])
-def set_tissue_options(selected_category):
-    return [{'label': i, 'value': i} for i in tcdict[selected_category]]
-
-@app.callback(
-    Output('tissue-dropdown', 'value'),
-    [Input('tissue-dropdown', 'options')])
-def set_tissue_value(available_options):
-    return available_options[0]['value']
-
-@app.callback(Output('gwas-graph', 'figure'), [Input('interaction-dropdown', 'value'), Input('tissue-dropdown', 'value')])
-def update_graph(selected_dropdown_value, selected_dropdown_value2):
-    #Here it would be possible to split df into 9 separate traces and return the list   
-    df = d_order[(d_order.Interaction==selected_dropdown_value) & (d_order.Tissue=="GWAS")]
-    #cols = df['PHE'].map(colorsIdx)
-    dl = []
-    for i in df.PHE.unique():
-        dfsub = df[df.PHE==i]
-        dff = {'x': dfsub.pos_index, 'y': dfsub.logp, 'mode': 'markers', 'text': dfsub.SNP + '\n' + dfsub.MAF, 'name': i}
+        #Text needs to be prefilled so add hover column here
+        dff = {'x': dfsub.pos_index, 'y': dfsub.logp, 'mode': 'markers', 'text': dfsub.SNP, 'name': i}
         dl.append(dff)
     return {
         'data': dl,
@@ -162,6 +154,7 @@ def update_graph(selected_dropdown_value, selected_dropdown_value2, selected_dro
     dl = []
     for i in df.PHE.unique():
         dfsub = df[df.PHE==i]
+        #Text needs to be prefilled so add hover column here
         dff = {'x': dfsub.pos_index, 'y': dfsub.logp, 'mode': 'markers', 'text': dfsub.SNP, 'name': i}
         dl.append(dff)
     return {
