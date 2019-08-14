04b72643508fc67ac8db67bf2c8c214f2a2d11c2
Anastasia Lucas <36303235+anastasia-lucas@users.noreply.github.com>
2019-08-14 14:17:35 -0400

    add checklist

--- personal/ana/scripts/manhattan_exPheWAS.py
+++ personal/ana/scripts/manhattan_exPheWAS.py
@@ -11,14 +11,14 @@ dat = pd.read_csv("~/Desktop/projects/plotly_dash/MetaXcan_eMERGEIII_UKBB_PheWAS
 dat['chromosome'] = dat.chromosome.astype(int)
 dat['gene_start_position'] = dat.gene_start_position.astype(int)
 dat['posterior_inclusion'] = dat.posterior_inclusion.astype(float)
-d_order = dat.sort_values(by=['chromosome','gene_start_position'])
+d_order = dat.sort_values(by=['category','chromosome','gene_start_position'])
 d_order['pos_index'] = range(1, len(d_order)+1)
 
 #Get tick positions
-maxRows = d_order.loc[d_order.groupby('chromosome').pos_index.agg('idxmax')][['chromosome', 'gene_start_position', 'pos_index']]
-minRows = d_order.loc[d_order.groupby('chromosome').pos_index.agg('idxmin')][['chromosome', 'gene_start_position', 'pos_index']]
-maxRows.columns = ['chrmax', 'posmax', 'posindmax']
-minRows.columns = ['chrmin', 'posmin', 'posindmin']
+maxRows = d_order.loc[d_order.groupby('category').pos_index.agg('idxmax')][['category','chromosome', 'gene_start_position', 'pos_index']]
+minRows = d_order.loc[d_order.groupby('category').pos_index.agg('idxmin')][['category','chromosome', 'gene_start_position', 'pos_index']]
+maxRows.columns = ['catmax', 'chr', 'posmax', 'posindmax']
+minRows.columns = ['catmin', 'chr', 'posmin', 'posindmin']
 lims = pd.concat([minRows.reset_index(), maxRows.reset_index()], axis=1)
 lims['av'] = lims[['posindmax', 'posindmin']].mean(axis=1)
 
@@ -35,7 +35,7 @@ d_order['Hover'] = "Gene: " + d_order['gene_name'] + "<br>Position: " + d_order[
 
 #Create shapes for chromosomes
 sl = []
-for i in range(int(len(limsshape.chrmin.unique()))):
+for i in range(int(len(limsshape.catmin.unique()))):
     shapes = {'type': 'rect', 'x0': limsshape.iloc[i]['posindmin'], 'x1': limsshape.iloc[i]['posindmax'], 'y0': ymin-0.5, 'y1': ymax, 'fillcolor': '#d3d3d3', 'opacity': 0.3, 'layer': 'below', 'line':{'width': 0}}
     sl.append(shapes)
 
@@ -58,6 +58,12 @@ tl = []
 for i in d_order.tissue.unique():
         tissues = {'label': i, 'value': i}
         tl.append(tissues)
+
+#Create list of chromosomes
+cl = []
+for i in d_order.chromosome.sort_values().unique():
+        chroms = {'label': i, 'value': i}
+        cl.append(chroms)
 
 #Get marker positions for slider based on max MAF
 markers={0: str(0), 0.1: str(0.1), 0.2: str(0.2), 0.3: str(0.3), 0.4: str(0.4), 0.5: str(0.5), 0.6: str(0.6), 0.7: str(0.7), 0.8: str(0.8), 0.9: str(0.9), 1: str(1)}
@@ -83,10 +89,6 @@ app.layout = html.Div([
     ],style={'width': '49%', 'display': 'inline-block', 'verticalAlign': 'top', 'horizontalAlign': 'right'}),
 
     html.Div([
-        dcc.Graph(id='twas-graph', style={'width': '180vh', 'height': '85vh', 'display': 'inline-block'})
-    ]),
-
-    html.Div([
         html.Label("Posterior Inclusion Probability Range:"),
         dcc.RangeSlider(
             id='postinc-slider',
@@ -97,13 +99,27 @@ app.layout = html.Div([
             value=[0,1],
             allowCross=False
         )
-    ],style={'height': '60px', 'width': '48%', 'display': 'inline-block', 'padding-left': '23%', 'padding-right': '23%'})
+    ],style={'height': '60px', 'width': '49%', 'display': 'inline-block', 'padding-right': '1%'}),
+
+    html.Div([
+        html.Label('Chromosome'),
+        dcc.Checklist(
+            id = 'chr-checklist',
+            options = cl,
+            values = d_order.chromosome.sort_values().unique(),
+            labelStyle = {'display': 'inline-block'}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'verticalAlign': 'top', 'horizontalAlign': 'left'}),
+
+    html.Div([
+        dcc.Graph(id='twas-graph', style={'width': '180vh', 'height': '85vh', 'display': 'inline-block'})
+    ])
 
 ])
 
-@app.callback(Output('twas-graph', 'figure'), [Input('tissue-dropdown', 'value'), Input('novelty-dropdown', 'value'), Input('postinc-slider', 'value')])
-def update_graph(selected_dropdown_value, selected_dropdown_value2, slider_value):
-    df = d_order[(d_order.novelty==selected_dropdown_value2) & (d_order.tissue==selected_dropdown_value) & (d_order.posterior_inclusion >= slider_value[0]) & (d_order.posterior_inclusion <= slider_value[1])]
+@app.callback(Output('twas-graph', 'figure'), [Input('tissue-dropdown', 'value'), Input('novelty-dropdown', 'value'), Input('chr-checklist', 'values'), Input('postinc-slider', 'value')])
+def update_graph(selected_dropdown_value, selected_dropdown_value2, chr_value, slider_value):
+    df = d_order[(d_order.novelty==selected_dropdown_value2) & (d_order.tissue==selected_dropdown_value) & (d_order.chromosome.isin(chr_value)) & (d_order.posterior_inclusion >= slider_value[0]) & (d_order.posterior_inclusion <= slider_value[1])]
     dsl = []
     for i in df.category.unique():
         dfsub = df[df.category==i]
@@ -112,7 +128,7 @@ def update_graph(selected_dropdown_value, selected_dropdown_value2, slider_value
         dsl.append(dff)
     return {
         'data': dsl,
-        'layout': {'shapes': sl, 'title': 'TWAS Plot', 'showlegend': True, 'xaxis': {'range': [xmin, xmax], 'ticktext': lims.chrmin, 'tickvals': lims.av, 'showgrid': False}, 'yaxis': {'range': [ymin,ymax], 'title': '-log10(pvalue)'}, 'hovermode': 'closest'}
+        'layout': {'shapes': sl, 'title': 'TWAS Plot', 'showlegend': True, 'xaxis': {'range': [xmin, xmax], 'showticklabels': False, 'tickvals': lims.av, 'showgrid': False}, 'yaxis': {'range': [ymin,ymax], 'title': '-log10(pvalue)'}, 'hovermode': 'closest'}
     }
 
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
